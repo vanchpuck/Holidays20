ebf92ab490cff5c6585016f9af45ad05767f8def
vanchpuck <vanchpuck@mail.ru>
2014-04-26 19:03:55 +0900

    13 версия (подправленная).

--- HolidaysCalendar20/src/com/jonnygold/holidays/fullcalendar/HolidaysActivity.java
+++ HolidaysCalendar20/src/com/jonnygold/holidays/fullcalendar/HolidaysActivity.java
@@ -16,7 +16,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteException;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.CalendarContract;
